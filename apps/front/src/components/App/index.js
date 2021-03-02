00e057b71750a388854e2cbbab038d6175bf08a3
Marie Morisi <m_morisi@yahoo.fr>
2021-03-02 15:50:41 +0100

    static login form and props done

--- apps/front/src/components/App/index.js
+++ apps/front/src/components/App/index.js
@@ -4,14 +4,26 @@ import React from 'react';
 // == Import
 import reactLogo from './react-logo.svg';
 import './styles.css';
+import LoginForm from '../LoginForm';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
-);
+const App = () => {
+  const handleLogin = () => {
+    console.log('handleLogin');
+  };
+  const handleInputChange = (evt) => {
+    const newValue = evt.target.value;
+    console.log(`handleInputChange + value :${newValue}`);
+  };
+  return (
+
+    <div className="app">
+      <img src={reactLogo} alt="react logo" />
+      <h1>Composant : App</h1>
+      <LoginForm handleLogin={handleLogin} email="test@gmail.fr" password="pass" handleInputChange={handleInputChange} />
+    </div>
+  );
+};
 
 // == Export
 export default App;
---
+++ apps/front/src/components/LoginForm/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+const LoginForm = ({
+  email, password, handleLogin, handleInputChange,
+}) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log('handleSubmit');
+    handleLogin();
+  };
+  return (
+
+    <div className="login-form">
+      <form
+        className="login-form-element"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="login-form-input"
+          type="email"
+          name="email"
+          placeholder="Votre email"
+          onChange={handleInputChange}
+          value={email}
+        />
+        <input
+          className="login-form-input"
+          type="password"
+          name="password"
+          placeholder="Votre password"
+          onChange={handleInputChange}
+          value={password}
+        />
+        <button className="login-form-submit" type="submit"> Valider </button>
+
+      </form>
+    </div>
+  );
+};
+
+LoginForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+
+};
+export default LoginForm;
