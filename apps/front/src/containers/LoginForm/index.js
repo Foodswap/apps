ccd56aef33b922fa5ac5ec3a0e01d83b26780d35
Marie Morisi <m_morisi@yahoo.fr>
2021-03-02 16:10:39 +0100

    loginForm container done with state props and function to dispatch

--- apps/front/src/containers/LoginForm/index.js
+++ apps/front/src/containers/LoginForm/index.js
@@ -1,2 +1,14 @@
 import { connect } from 'react-redux';
 import LoginForm from '../../components/LoginForm';
+
+const mapStateToProps = (state) => ({
+  email: state.userReducer.email,
+  password: state.userReducer.password,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleInputChange: (value, name) => dispatch(setInputValue(value, name)),
+  handleLogin: () => dispatch(sendLogin()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
