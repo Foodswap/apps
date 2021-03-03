f0542830c2018e2a9f5f9edef305da0fc04db03f
Marie Morisi <m_morisi@yahoo.fr>
2021-03-03 13:49:01 +0100

    basic css

--- apps/front/src/components/SignUpForm/index.js
+++ apps/front/src/components/SignUpForm/index.js
@@ -11,57 +11,59 @@ const SignUp = ({
     handleSignUp();
   };
   return (
+    <div className="signup-modale">
 
-    <div className="signup-form">
-      <h2 className="signup-title">
-        Inscrivez-vous
-      </h2>
-      <form
-        className="signup-form-element"
-        onSubmit={handleSubmit}
-      >
-        <input
-          className="signup-form-input"
-          type="email"
-          name="email"
-          placeholder="Votre email"
-          onChange={(evt) => {
-            handleInputChange(evt.target.value, evt.target.name);
-          }}
-          value={email}
-        />
-        <input
-          className="signup-form-input"
-          type="password"
-          name="password"
-          placeholder="Votre mot de passe"
-          onChange={(evt) => {
-            handleInputChange(evt.target.value, evt.target.name);
-          }}
-          value={password}
-        />
-        <input
-          className="signup-form-input"
-          type="pseudo"
-          name="pseudo"
-          placeholder="pseudo"
-          onChange={(evt) => {
-            handleInputChange(evt.target.value, evt.target.name);
-          }}
-          value={pseudo}
-        />
-        <input
-          className="signup-form-input"
-          type="city"
-          name="city"
-          placeholder="city"
-          onChange={(evt) => {
-            handleInputChange(evt.target.value, evt.target.name);
-          }}
-          value={city}
-        />
-        <button className="signup-form-submit" type="submit"> Valider </button>
-      </form>
+      <div className="signup-form">
+        <h2 className="signup-title">
+          Inscrivez-vous
+        </h2>
+        <form
+          className="signup-form-element"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="signup-form-input"
+            type="email"
+            name="email"
+            placeholder="Votre email"
+            onChange={(evt) => {
+              handleInputChange(evt.target.value, evt.target.name);
+            }}
+            value={email}
+          />
+          <input
+            className="signup-form-input"
+            type="password"
+            name="password"
+            placeholder="Votre mot de passe"
+            onChange={(evt) => {
+              handleInputChange(evt.target.value, evt.target.name);
+            }}
+            value={password}
+          />
+          <input
+            className="signup-form-input"
+            type="pseudo"
+            name="pseudo"
+            placeholder="pseudo"
+            onChange={(evt) => {
+              handleInputChange(evt.target.value, evt.target.name);
+            }}
+            value={pseudo}
+          />
+          <input
+            className="signup-form-input"
+            type="city"
+            name="city"
+            placeholder="city"
+            onChange={(evt) => {
+              handleInputChange(evt.target.value, evt.target.name);
+            }}
+            value={city}
+          />
+          <button className="signup-form-submit" type="submit"> Valider </button>
+        </form>
+      </div>
     </div>
   );
 };
