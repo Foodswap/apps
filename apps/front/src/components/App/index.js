7dcdbc8c6d2678641787140db1378f86e4398f4d
Vang-ap <pliaannevang.02@gmail.com>
2021-03-03 20:23:28 +0100

    change the size in the App folder on the index.js and style.scss file

--- apps/front/src/components/App/index.js
+++ apps/front/src/components/App/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 // == Import
 import AppHeader from '../AppHeader';
 import Menu from '../../containers/Menu';
+import DescriptionHomepage from '../DescriptionHomepage';
 import LoginForm from '../../containers/LoginForm';
 import SignUpForm from '../../containers/SignUpForm';
 import Footer from '../Footer';
@@ -17,6 +18,7 @@ const App = () => (
   <div className="app">
     <AppHeader />
     <Menu />
+    <DescriptionHomepage />
     <LoginForm />
     <SignUpForm />
     <Footer />
