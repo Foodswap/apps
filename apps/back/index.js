9203a313288c4c44c00d5e0c7391df33c574fb3b
edouardLaffont <edouard.laffont@live.com>
2021-03-03 10:51:51 +0100

    database test OK, router created and index link with, dotenv config OK

--- apps/back/index.js
+++ apps/back/index.js
@@ -6,6 +6,18 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const router = require('./app/router');
+
+app.use(express.json());
+
+app.use('/v1', router);
+
+/* Test front-back
+app.get('/', async (req, res) => {
+   res.send('Hello');
+});
+*/
+
 app.listen(port, _ => {
    console.log(`http://localhost:${port}`);
 });
