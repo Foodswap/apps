51a9e4c2dd4c8fdcf5f86627b1c10f20cf80671e
Marie Morisi <m_morisi@yahoo.fr>
2021-03-08 11:54:08 +0100

    link to dynamic with id

--- apps/front/src/components/LastDishes/index.js
+++ apps/front/src/components/LastDishes/index.js
@@ -10,17 +10,20 @@ const LastDishes = ({ dishes }) => (
     <h2 className="last-dishes-title">Les derniers plats ajoutés</h2>
     <div className="last-dishes-cards">
       {
-      dishes.map((dish) => (
-        <div className="last-dishes-card" key={dish.id}>
-          <img className="last-dishes-card-img" src={dish.picture} alt="" />
-          <h3 className="last-dishes-card-name">{dish.name}</h3>
-          <p className="last-dishes-card-potion">{dish.portion} part(s)</p>
-          <p className="last-dishes-card-author"> Fait par {dish.author.pseudonym}</p>
-          <p className="last-dishes-card-city">{dish.city}</p>
-          <Link to="/dish/{dish.id}" className="last-dishes-card-seemore"> Voir plus </Link>
-        </div>
+      dishes.map((dish) => {
+        const linkUrl = `/dish/${dish.id}`;
+        return (
 
-      ))
+          <div className="last-dishes-card" key={dish.id}>
+            <img className="last-dishes-card-img" src={dish.picture} alt="" />
+            <h3 className="last-dishes-card-name">{dish.name}</h3>
+            <p className="last-dishes-card-potion">{dish.portion} part(s)</p>
+            <p className="last-dishes-card-author"> Fait par {dish.author.pseudonym}</p>
+            <p className="last-dishes-card-city">{dish.city}</p>
+            <Link to={linkUrl} className="last-dishes-card-seemore"> Voir plus </Link>
+          </div>
+        );
+      })
     }
     </div>
   </div>
