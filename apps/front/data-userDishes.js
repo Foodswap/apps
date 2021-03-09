5d0fe57d76cad0ff03730c863a4e98db648c0d57
Vang-ap <pliaannevang.02@gmail.com>
2021-03-09 12:05:34 +0100

    Ingredients added  and description modification ( of array at ES6)

--- apps/front/data-userDishes.js
+++ apps/front/data-userDishes.js
@@ -3,9 +3,43 @@ export default [
     id: 1,
     picture: 'https://lh3.google.com/u/0/d/1lll7XWtpAM_tWXcCuHgWfncuFULfwg10=w842-h770-iv1',
     name: 'Buddha bowl',
-    description: [
-      'une basse riz.',
-      ' Sur le dessus il y a du saumon, avocat, carotte, choux rouge',
+    description: `une basse riz.
+        Sur le dessus il y a du saumon, avocat, carotte, choux rouge`,
+    ingredients: [
+      {
+        id: 1,
+        name: 'riz',
+      },
+
+      {
+        id: 2,
+        name: 'saumon',
+      },
+
+      {
+        id: 3,
+        name: 'carotte',
+      },
+
+      {
+        id: 4,
+        name: 'avocat',
+      },
+
+      {
+        id: 5,
+        name: 'graine de sésame',
+      },
+
+      {
+        id: 6,
+        name: 'choux rouge',
+      },
+
+      {
+        id: 7,
+        name: 'citron',
+      },
     ],
     created_date: '12.03.2021',
     portion: '1',
@@ -20,9 +54,43 @@ export default [
     id: 2,
     picture: 'https://lh3.google.com/u/0/d/1BZu0wwMKsAsSwxuob8idG-RgMOpLUy9d=w275-h786-iv2',
     name: 'Kimbap et bibimbap',
-    description: [
-      'une basse riz.',
-      ' Sur le dessus il y a du boeuf haché, épinard, carotte, oeuf',
+    description: `une basse riz.',
+      ' Sur le dessus il y a du boeuf haché, épinard, carotte, oeuf`,
+    ingredients: [
+      {
+        id: 9,
+        name: 'riz',
+      },
+
+      {
+        id: 10,
+        name: 'boeuf haché',
+      },
+
+      {
+        id: 11,
+        name: 'carotte',
+      },
+
+      {
+        id: 12,
+        name: 'épinard',
+      },
+
+      {
+        id: 13,
+        name: 'graine de sésame',
+      },
+
+      {
+        id: 14,
+        name: 'oeuf',
+      },
+
+      {
+        id: 15,
+        name: 'oignon',
+      },
     ],
     created_date: '12.03.2021',
     portion: '3',
@@ -37,9 +105,43 @@ export default [
     id: 3,
     picture: 'https://lh3.google.com/u/0/d/1X167WvnTUO-8g7Yu5OpSY5tJNQrawiLi=w275-h786-iv1',
     name: 'Number cake',
-    description: [
-      'une basse génoise, fouré d\'une ganache monté chocolat blanc et fraise.',
-      ' Sur le dessus il y a de la crème chantilly, macaron, fraise et fleures en sucre',
+    description: `une basse génoise, fouré d'une ganache monté chocolat blanc et fraise.
+      Sur le dessus il y a de la crème chantilly, macaron, fraise et fleures en sucre`,
+    ingredients: [
+      {
+        id: 16,
+        name: 'farine',
+      },
+
+      {
+        id: 17,
+        name: 'sucre',
+      },
+
+      {
+        id: 18,
+        name: 'crème liquide',
+      },
+
+      {
+        id: 19,
+        name: 'oeuf',
+      },
+
+      {
+        id: 20,
+        name: 'fix chantilly',
+      },
+
+      {
+        id: 21,
+        name: 'fraise',
+      },
+
+      {
+        id: 22,
+        name: 'maizena',
+      },
     ],
     created_date: '09.03.2021',
     portion: '10',
@@ -54,9 +156,44 @@ export default [
     id: 4,
     picture: 'https://lh3.google.com/u/0/d/17KI1SHK9GJYZkoesZ9KQmy4e6DLIXXnb=w275-h786-iv1',
     name: 'Nems',
-    description: [
-      'une basse feuille pour nems.',
-      ' A l\'intéreur on retrouve une farce porc haché, carotte, champignon noir et vermicelle',
+    description: ` une basse feuille pour nems.
+       A l'intéreur on retrouve une farce porc haché, carotte, champignon noir et vermicelle`,
+    ingredientss: [
+      {
+        id: 23,
+        name: 'vermicelle',
+      },
+
+      {
+        id: 24,
+        name: 'viande de porc haché',
+      },
+
+      {
+        id: 25,
+        name: 'carotte',
+      },
+
+      {
+        id: 26,
+        name: 'champignon noir',
+      },
+
+      {
+        id: 27,
+        name: 'fécule de pomme de terre',
+      },
+
+      {
+        id: 28,
+        name: 'fécule de pomme de terre',
+      },
+
+      {
+        id: 29,
+        name: 'feuilles de samoussa',
+      },
+
     ],
     created_date: '03.03.2021',
     portion: '6',
@@ -71,9 +208,44 @@ export default [
     id: 5,
     picture: 'https://lh3.google.com/u/0/d/1xYWT2_iv_KwkvON_b2r0M69HrT3BHSDB=w275-h786-iv1',
     name: 'Soupe thai',
-    description: [
-      'une basse de nouille et un bouillon lait de coco curry rouge.',
-      ' Sur le dessus on retrouve des raviolis frits, des crevettes, des champignons shiitake et oignon rouge',
+    description: `une basse de nouille et un bouillon lait de coco curry rouge.
+       Sur le dessus on retrouve des raviolis frits, des crevettes, des champignons shiitake et oignon rouge`,
+    ingredients: [
+      {
+        id: 30,
+        name: 'vermicelle',
+      },
+
+      {
+        id: 31,
+        name: 'crevette',
+      },
+
+      {
+        id: 32,
+        name: 'raviolis frit',
+      },
+
+      {
+        id: 33,
+        name: 'champignon shiitake',
+      },
+
+      {
+        id: 34,
+        name: 'lait de coco',
+      },
+
+      {
+        id: 35,
+        name: 'pate de curry rouge',
+      },
+
+      {
+        id: 36,
+        name: 'oignon rouge',
+      },
+
     ],
     created_date: '14.03.2021',
     portion: '2',
