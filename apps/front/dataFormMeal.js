6ef49eb1ab75051c3a36607b4c0461b24f48a351
Cyril-Promo-N <cyrilrousteau@gmail.com>
2021-03-09 13:23:20 +0100

    Import fake data for dishesForm

---
+++ apps/front/dataFormMeal.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    type: 'Type de cuisine',
+    name: 'Francaise',
+
+  },
+  {
+    id: 2,
+    type: 'Type de cuisine',
+    name: 'Indienne',
+  },
+  {
+    id: 3,
+    type: 'Type de cuisine',
+    name: 'Asiatique',
+  },
+  {
+
+    id: 4,
+    type: 'Type de cuisine',
+    name: 'Italienne',
+
+  },
+  {
+    id: 5,
+    type: 'Type de cuisine',
+    name: 'Greque',
+  },
+  {
+    id: 6,
+    type: 'Type de cuisine',
+    name: 'Africaine',
+  },
+
+];
