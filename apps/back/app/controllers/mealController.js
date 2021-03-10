47f38446274756c1c41d745ae7ffeaa9ee138d54
edouardLaffont <edouard.laffont@live.com>
2021-03-10 11:49:34 +0100

    creation meal OK without associate

--- apps/back/app/controllers/mealController.js
+++ apps/back/app/controllers/mealController.js
@@ -6,8 +6,9 @@ const mealController = {
         const meal = new Meal(request.body);
         try {
             meal.picture_path = `${process.env.PATH_PICTURE}/${request.file.filename}`
-            console.log(meal.picture_path);
-            response.status(200).json("bravo")
+            await meal.save();
+            meal.picture_path = null;
+            response.status(200).json(meal);
         } catch(error) {
             console.log(error);
 
