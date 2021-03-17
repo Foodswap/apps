fdac78423024a3601c5e32b8d84c4e1b798775ee
Cyril-Promo-N <cyrilrousteau@gmail.com>
2021-03-17 16:43:15 +0100

    Css in Faces component part 1

--- apps/front/src/components/App/index.js
+++ apps/front/src/components/App/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+
 // Dumb components
 import AppHeader from '../AppHeader';
 import Footer from '../Footer';
--- apps/front/src/components/Faces/index.js
+++ apps/front/src/components/Faces/index.js
@@ -1,4 +1,9 @@
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,800,300' rel='stylesheet' type='text/css'></link>
+
+
 import React from 'react';
+import { FaReact, FaGithub } from 'react-icons/fa';
+import { SiPostgresql, SiJavascript } from 'react-icons/si';
 import './style.scss';
 import image from '../../assets/images/image.png';
 import image1 from '../../assets/images/image (1).png';
@@ -9,10 +14,24 @@ import image4 from '../../assets/images/image (4).png';
 const Faces = () => (
 
   <div className="face-page">
-    <div className="face-title">Qui sommes-nous ?</div>
-    <section className="face-container">
+    <div className="face-container">
+      <div className="face-title">
+        À propos de nous ...
+      </div>
+      <p className="face-paragraph">
+        Nous sommes cinq développeurs.euses Web junior.
+      </p>
+      <p className="face-paragraph">À l'issue de notre formation de développeur Web Fullstack chez O'clock, nous vous proposons cet univers culinaire cuisiné par nos propres soins.</p>
+      <p className="face-paragraph">Ce site est le fruit d'un travail cuisant durant 5 mois d'apprentissage et d'aventure humaine intense.</p>
+      <p className="face-paragraph_p">Nos principaux ingrédients ?</p>
+      <p className="face-paragraph">Une touche de Node.JS aux petits oignons pour le back-end et une pinçée de gros sel chez React pour le front-end. </p>
+      <p className="face-paragraph">Ajoutez un soupçon de matière grise venant d'une brigade sur-motivées, et vous obtenez ce merveilleux projet d'apothéose !</p>
+   </div>
+    
+    
+    {/* <section className="face-pictures">
       <figure>
-        <img className="face-image" src={image1} alt="Plia" />
+        <img className="face-images" src={image1} alt="Plia" />
         <figcaption className="face-titre-image">Plia Product Owner</figcaption>
       </figure>
       <figure>
@@ -27,11 +46,108 @@ const Faces = () => (
         <img className="face-image" src={image3} alt="Edouard" />
         <figcaption className="face-titre-image">Edouard Lead dev back</figcaption>
       </figure>
-      <figure>
-        <img className="face-image" src={image2} alt="Florian" />
-        <figcaption className="face-titre-image">Florian Git Master</figcaption>
-      </figure>
-    </section>
+      </section> */}
+      <div className="bigcontent">
+      <div className="content">
+      <h2>Edouard</h2>
+        <div className="grid">
+          <figure className="effect-zoe">
+            <img src={image3} alt="Edouard"/>
+            <figcaption>
+              <h2>Back-end <span>Lead dev Back</span></h2>
+              <p className="icon-links">
+                <a href="#"><span className="icon-git"></span><FaGithub /></a>
+                <a href="#"><span className="icon-js"><SiJavascript /></span></a>
+                <a href="#"><span className="icon-sql-clip"><SiPostgresql /></span></a>
+              </p>
+              <p className="description">Edouard, Lead-Dev Back est un codeur hors pair ! Sa bonne humeur ajoute à cette équipe un parfait équilibre.</p>
+              
+            </figcaption>     
+          </figure>
+          
+        </div>
+        </div>
+
+
+        <div className="content">
+      <h2>Plia</h2>
+        <div className="grid">
+          <figure className="effect-zoe">
+            <img src={image1} alt="Plia"/>
+            <figcaption>
+              <h2>Front-end <span>Product Owner</span></h2>
+              <p className="icon-links">
+                <a href="#"><span className="icon-react"><FaReact /></span></a>
+                <a href="#"><span className="icon-git"><FaGithub /></span></a>
+                <a href="#"><span className="icon-js"><SiJavascript /></span></a>
+              </p>
+              <p className="description">Plia est notre Product Owner.Elle est à l'origine de ce magnifique projet.Son punch et sa débordante imagination nous pousse vers de nouveaux chalenges !</p>
+            </figcaption>     
+          </figure>
+          
+        </div>
+        </div>
+
+
+        <div className="content">
+      <h2>Marie</h2>
+        <div className="grid">
+          <figure className="effect-zoe">
+            <img src={image} alt="Marie"/>
+            <figcaption>
+              <h2>Front-end <span>Scrum Master</span></h2>
+              <p className="icon-links">
+              
+                <a href="#"><span className="icon-react"><FaReact /></span></a>
+                <a href="#"><span className="icon-git"><FaGithub /></span></a>
+                <a href="#"><span className="icon-js"><SiJavascript /></span></a>
+              </p>
+              <p className="description">Marie est notre Scrum Master.Elle maîtrise parfaitement le code.Aucune issue ne lui résiste ! Sa douceur apporte la zénitude quand nos esprits bouillonnent !</p>
+            </figcaption>     
+          </figure>
+          
+        </div>
+        </div>
+
+        <div className="content">
+      <h2>Florian</h2>
+        <div className="grid">
+          <figure className="effect-zoe">
+            <img src={image2} alt="Florian"/>
+            <figcaption>
+              <h2>Back-end <span>Git Master</span></h2>
+              <p className="icon-links">
+                <a href="#"><span className="icon-git"><FaGithub /></span></a>
+                <a href="#"><span className="icon-js"><SiJavascript /></span></a>
+                <a href="#"><span className="icon-sql-clip"><SiPostgresql /></span></a>
+              </p>
+              <p className="description">Florian est un maître dans la maîtrise de Git. Il faut un sang-froid démeusuré pour gérer la lourde responsabilité des merges. Un pilier !</p>
+            </figcaption>     
+          </figure>
+          
+        </div>
+        </div>
+
+        <div className="content">
+          <h2>Cyril</h2>
+            <div className="grid">
+              <figure className="effect-zoe">
+                <img src={image4} alt="Cyril"/>
+               <figcaption>
+                 <h2>Front-end <span>Lead Dev Front</span></h2>
+                  <p className="icon-links">
+                    <a href="#"><span className="icon-react"><FaReact /></span></a>
+                    <a href="#"><span className="icon-git"><FaGithub /></span></a>
+                    <a href="#"><span className="icon-js"><SiJavascript /></span></a>
+                  </p>
+                  <p className="description">Codeur du bout du monde, de part sa détermination et son écoute, Cyril apporte ses idées et suggestions.De tempérament calme, il contribue à une entente détendue.</p>
+               </figcaption>     
+             </figure>
+          
+            </div>
+        </div>
+        </div>
+    
 
   </div>
 );
