93b77417601c014b140349dcf0011b631c79b68d
Marie Morisi <m_morisi@yahoo.fr>
2021-03-26 17:39:32 +0100

    style: fix lint

--- apps/back/src/controllers/authorController.js
+++ apps/back/src/controllers/authorController.js
@@ -1,20 +1,20 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const Author = require("../models/author");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Author = require('../models/author');
 
 const authorController = {
   getOneAuthor: async (request, response) => {
     try {
       const author = await Author.findByPk(Number(request.params.id), {
         attributes: {
-          exclude: ["password"],
+          exclude: ['password'],
         },
       });
 
       if (author) {
         response.status(200).json(author);
       } else {
-        response.status(404).json({ error: 404, message: "User not found" });
+        response.status(404).json({ error: 404, message: 'User not found' });
       }
     } catch (err) {
       response.status(500).json(err);
@@ -30,7 +30,7 @@ const authorController = {
       });
 
       if (userEmail) {
-        return response.status(409).json("Email déjà enregistré");
+        return response.status(409).json('Email déjà enregistré');
       }
 
       const userName = await Author.findOne({
@@ -38,7 +38,7 @@ const authorController = {
       });
 
       if (userName) {
-        return response.status(409).json("Nom utilisateur déjà enregistré");
+        return response.status(409).json('Nom utilisateur déjà enregistré');
       }
 
       author.password = bcrypt.hashSync(request.body.password, 10);
@@ -63,19 +63,18 @@ const authorController = {
       });
 
       if (
-        !author ||
-        !bcrypt.compareSync(request.body.password, author.password)
+        !author || !bcrypt.compareSync(request.body.password, author.password)
       ) {
-        return response.status(401).json("Email ou mdp non valide.");
+        return response.status(401).json('Email ou mdp non valide.');
       }
       author.password = null;
 
       const accessToken = `Bearer ${jwt.sign(
         { username: author.username },
-        accessTokenSecret
+        accessTokenSecret,
       )}`;
 
-      return response.set("Authorization", accessToken).json({
+      return response.set('Authorization', accessToken).json({
         author,
       });
     } catch (err) {
