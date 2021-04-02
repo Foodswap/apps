18addd7846395cb0acda63adbb77e3e267a4b8fc
Marie Morisi <m_morisi@yahoo.fr>
2021-04-02 10:39:23 +0200

    feat(swagger-api-doc): add swagger doc on get category

--- apps/back/src/controllers/categoryController.js
+++ apps/back/src/controllers/categoryController.js
@@ -1,6 +1,42 @@
 const Category = require('../models/category');
 
 const categoryController = {
+  /**
+   * GET /v1/categories/{type}
+   *
+   * @summary Get Category by type
+   * @tags Category
+   *
+   * @param {string} type.path - type of Category
+   *
+   * @return {AuthorDto} 200 - success response - application/json
+   * @return {ErrorDto} 404 - bad request response
+   * @return {ErrorDto} 500 - error on server
+   *
+   * @example response - 200 - a category by kitchen type returns by api
+   * {
+   *   "id": "1",
+   *   "type": "kitchen",
+   *   "name": "Asiatique"
+   * }
+   * @example response - 200 - a category by dish type returns by api
+   * {
+   *   "id": "2",
+   *   "type": "dish",
+   *   "name": "Plat"
+   * }
+   * @example response - 404 - an error of bad request
+   * {
+   *   "error": 404,
+   *   "message": "Category not found"
+   * }
+   * @example response - 500 - an error on server
+   * {
+   *   "error": 500,
+   *   "message": "Internal server error"
+   * }
+   */
+
   getCategories: async (request, response) => {
     const { type } = request.params;
     const query = {
