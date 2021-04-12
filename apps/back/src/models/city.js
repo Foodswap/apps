8a24979d2d892d82dadefb4371c36d65d3d2bfe0
Marie Morisi <m_morisi@yahoo.fr>
2021-04-12 15:44:58 +0200

    feat: create city Model

---
+++ apps/back/src/models/city.js
@@ -0,0 +1,51 @@
+const { Model } = require('sequelize');
+
+/**
+ * A City
+ *
+ * @typedef {object} CityBody
+ *
+ * @property {string} county.required - The county
+ * @property {string} slug.required - The slug
+ * @property {string} name.required - The name
+ * @property {string} zip_code.required - The zip code
+ * @property {integer} district.required - The district
+ * @property {float} longitude.required - The longitude
+ * @property {float} latitude.required - The latitude
+ */
+
+class City extends Model {
+  static ini(sequelize, DataTypes) {
+    return super.init(
+      {
+        county: {
+          type: DataTypes.STRING,
+        },
+        slug: {
+          type: DataTypes.STRING,
+        },
+        name: {
+          type: DataTypes.STRING,
+        },
+        zip_code: {
+          type: DataTypes.STRING,
+        },
+        district: {
+          type: DataTypes.INTEGER,
+        },
+        longitude: {
+          type: DataTypes.FLOAT,
+        },
+        latitude: {
+          type: DataTypes.FLOAT,
+        },
+      },
+      {
+        sequelize,
+        tableName: 'city',
+      },
+    );
+  }
+}
+
+module.exports = City;
