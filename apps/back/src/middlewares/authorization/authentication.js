27975684d940b70f3a3d9c3bb3f5962f5a4a0675
Plia Vang <pliaannevang.02@gmail.com>
2021-04-20 18:01:50 +0200

    fix(security-for-dish-online): fix commentary #41
    remove console.log on authentication

--- apps/back/src/middlewares/authorization/authentication.js
+++ apps/back/src/middlewares/authorization/authentication.js
@@ -4,8 +4,6 @@ const authenticateJWT = (request, response, next) => {
   const authHeader = request.headers.authorization;
   const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
-  console.log(authHeader);
-
   if (authHeader) {
     const token = authHeader.split(' ')[1];
 
