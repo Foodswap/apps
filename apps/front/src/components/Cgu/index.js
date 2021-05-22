48dbfa631a31c7d3437b9c3d94de24debe03b566
Plia <65957915+Vang-ap@users.noreply.github.com>
2021-05-22 10:10:48 +0200

    feat(add-cgu): add cgu in footer #45 (#61)
    * feat(add-cgu): add cgu in footer #45
    
    add cgu conditions générales d'utilisation in the footer. creat page cgu (conponent)
    
    * fix: test
    
    Co-authored-by: Vang-ap <neilnilom@gmail.com>

---
+++ apps/front/src/components/Cgu/index.js
@@ -0,0 +1,399 @@
+import React from 'react';
+import './style.scss';
+
+const Cgu = () => (
+  <div className="cgu">
+    <div className="cgu-page-content">
+      <div className="cgu-header">
+        <h1 className="cgu-header-main-title">Conditions générales d'utilisation</h1>
+        <p className="cgu-header-abbreviation">(CGU)</p>
+        <h2 className="cgu-header-effective-date">En vigueur au 21/04/2021</h2>
+      </div>
+
+      <div className="cgu-text-content">
+        <div className="cgu-text-content-header">
+          <p className="cgu-text">
+            Les présentes conditions générales d'utilisation (dites « CGU »)
+          </p>
+
+          <p className="cgu-text">
+            ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par _______________  et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».
+          </p>
+
+          <p className="cgu-text">
+            Les présentes CGU sont accessibles sur le site à la rubrique «CGU».
+          </p>
+
+          <p className="cgu-text">
+            Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur.
+          </p>
+
+          <p className="cgu-text">
+            Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant :
+          </p>
+
+          <p className="cgu-text">
+            « Je reconnais avoir lu et compris les CGU et je les accepte ».
+          </p>
+
+          <p className="cgu-text">
+            En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
+          </p>
+
+          <p className="cgu-text">
+            foodswap.fr  se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            Article 1 : Les mentions légales
+          </h3>
+
+          <div className="cgu-text-article-party1">
+            <p className="cgu-text">
+              L’édition et la direction de la publication du site foodswap.fr est assurée par Vang Plia Anne,
+            </p>
+
+            <p className="cgu-text">
+              domicilié 31 Rue des Cotes Blanches.
+            </p>
+
+            <p className="cgu-text">
+              Numéro de téléphone est 0650603967
+            </p>
+
+            <p className="cgu-text">
+              Adresse e-mail pliaannevang.02@gmail.com.
+            </p>
+          </div>
+
+          <div className="cgu-text-article-party2">
+            <p className="cgu-text">
+              L'hébergeur du site foodswap.fr est la société GANDI SAS, dont le siège social est situé au 63-65 boulevard Masséna - 75013 Paris,
+            </p>
+
+            <p className="cgu-text">
+              avec le numéro de téléphone : 0170377661.
+            </p>
+          </div>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 2 : Accès au site
+          </h3>
+
+          <div className="cgu-text-article-party1">
+            <p className="cgu-text">
+              Le site foodswap.fr  permet à l'Utilisateur un accès gratuit aux services suivants :
+            </p>
+
+            <p className="cgu-text">
+              Le site internet propose les services suivants :
+            </p>
+
+            <p className="cgu-text">
+              service d'échange de plats entre particuliers
+            </p>
+
+            <p className="cgu-text">
+              Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet.
+            </p>
+
+            <p className="cgu-text">
+              Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
+            </p>
+          </div>
+
+          <div className="cgu-text-article-party2">
+            <p className="cgu-text">
+              L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire.
+            </p>
+
+            <p className="cgu-text">
+              En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes
+            </p>
+
+            <p className="cgu-text">
+              concernant son état civil et ses coordonnées, notamment son adresse email.
+            </p>
+
+            <p className="cgu-text">
+              Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.
+            </p>
+
+            <p className="cgu-text">
+              Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.
+            </p>
+
+            <p className="cgu-text">
+              Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification
+            </p>
+
+            <p className="cgu-text">
+              en cas de maintenance, n'engage pas la responsabilité de foodswap.fr. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à
+            </p>
+
+            <p className="cgu-text">
+              l’éditeur de toute interruption ou suspension de service, même sans préavis.
+
+            </p>
+
+            <p className="cgu-text">
+              L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.
+            </p>
+
+          </div>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 3 : Collecte des données
+          </h3>
+
+          <p className="cgu-text">
+            Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL)
+          </p>
+
+          <p className="cgu-text">
+            dans la mesure où il ne collecte aucune donnée concernant les Utilisateurs.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 4 : Propriété intellectuelle
+          </h3>
+
+          <div className="cgu-text-article-party1">
+            <p className="cgu-text">
+              Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…)
+            </p>
+
+            <p className="cgu-text">
+              font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
+            </p>
+          </div>
+
+          <div className="cgu-text-article-party2">
+            <p className="cgu-text">
+              L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.
+            </p>
+
+            <p className="cgu-text">
+              Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.
+            </p>
+
+            <p className="cgu-text">
+              Toute représentation totale ou partielle de ce site par quelque procédé que ce soit,
+            </p>
+
+            <p className="cgu-text">
+              sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.
+            </p>
+
+            <p className="cgu-text">
+              Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.
+            </p>
+          </div>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 5 : Responsabilité
+          </h3>
+
+          <p className="cgu-text">
+            Les sources des informations diffusées sur le site foodswap.fr sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.
+          </p>
+
+          <p className="cgu-text">
+            Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle.
+          </p>
+
+          <p className="cgu-text">
+            Malgré des mises à jour régulières, le site foodswap.fr ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication.
+          </p>
+
+          <p className="cgu-text">
+            De même, le site ne peut être tenue responsable de l’utilisation
+          </p>
+
+          <p className="cgu-text">
+            et de l’interprétation de l’information contenue dans ce site.
+          </p>
+
+          <p className="cgu-text">
+            L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite.
+          </p>
+
+          <p className="cgu-text">
+            Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+          </p>
+
+          <p className="cgu-text">
+            Le site foodswap.fr ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute,
+          </p>
+
+          <p className="cgu-text">
+            suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.
+          </p>
+
+          <p className="cgu-text">
+            La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 6 : Liens hypertextes
+          </h3>
+
+          <p className="cgu-text">
+            Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens,
+          </p>
+
+          <p className="cgu-text">
+            il sortira du site foodswap.fr. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait,
+          </p>
+
+          <p className="cgu-text">
+            en aucun cas, être responsable de leur contenu.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 7 : Cookies
+          </h3>
+
+          <p className="cgu-text">
+            L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.
+          </p>
+
+          <p className="cgu-text">
+            Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et
+          </p>
+
+          <p className="cgu-text">
+            et qui sont nécessaires à l’utilisation du site foodswap.fr. Les cookies ne contiennent pas d’information personnelle et
+          </p>
+
+          <p className="cgu-text">
+            ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique,
+          </p>
+
+          <p className="cgu-text">
+            généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.
+          </p>
+
+          <p className="cgu-text">
+            L’information contenue dans les cookies est utilisée pour améliorer le site foodswap.fr.
+          </p>
+
+          <p className="cgu-text">
+            En naviguant sur le site, L’Utilisateur les accepte.
+          </p>
+
+          <p className="cgu-text">
+            L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
+          </p>
+
+          <p className="cgu-text">
+            A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.
+          </p>
+
+          <p className="cgu-text">
+            L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 8 : Publication par l’Utilisateur
+          </h3>
+
+          <p className="cgu-text">
+            Le site permet aux membres de publier les contenus suivants :
+
+          </p>
+
+          <p className="cgu-text">
+            photo et texte.
+          </p>
+
+          <p className="cgu-text">
+            Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.
+          </p>
+
+          <p className="cgu-text">
+            Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.
+          </p>
+
+          <p className="cgu-text">
+            Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site,
+          </p>
+
+          <p className="cgu-text">
+            il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier,
+          </p>
+
+          <p className="cgu-text">
+            diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support
+          </p>
+
+          <p className="cgu-text">
+            (numérique ou physique), pour la durée de la propriété intellectuelle. Le Membre cède notamment le droit d'utiliser sa publication sur internet et sur les réseaux de téléphonie mobile.
+          </p>
+
+          <p className="cgu-text">
+            La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.
+          </p>
+
+          <p className="cgu-text">
+            Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité.
+          </p>
+
+          <p className="cgu-text">
+            L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes.
+          </p>
+
+          <p className="cgu-text">
+            Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.
+          </p>
+
+          <p className="cgu-text">
+            Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site, sans préavis.
+          </p>
+        </div>
+
+        <div className="cgu-text-content-article">
+          <h3 className="cgu-text-title">
+            ARTICLE 9 : Droit applicable et juridiction compétente
+          </h3>
+
+          <p className="cgu-text">
+            La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties,
+          </p>
+
+          <p className="cgu-text">
+            les tribunaux français seront seuls compétents pour en connaître.
+          </p>
+
+          <p className="cgu-text">
+            Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.
+          </p>
+        </div>
+      </div>
+
+      <div className="cgu-footer">
+        <p className="cgu-text">
+          CGU réalisées sur http://legalplace.fr/
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Cgu;
